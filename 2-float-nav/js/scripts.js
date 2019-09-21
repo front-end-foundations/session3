8daeb3837c6d128e3a24511b583715f4f14682ca
DannyBoyNYC <daniel.deverell@gmail.com>
2019-09-21 11:48:05 -0400

    done

--- 2-float-nav/js/scripts.js
+++ 2-float-nav/js/scripts.js
@@ -1,38 +1,62 @@
 // scripts go here
 
-var cuisines =
-  'Cuisines. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio maiores adipisci quibusdam repudiandae dolor vero placeat esse sit! Quibusdam saepe aperiam explicabo placeat optio, consequuntur nihil voluptatibus expedita quia vero perferendis, deserunt et incidunt eveniet temporibus doloremque possimus facilis.';
+// fetch(
+//   'https://api.nytimes.com/svc/topstories/v2/science.json?api-key=uQG4jhIEHKHKm0qMKGcTHqUgAolr1GM0',
+// )
+//   .then(response => response.json())
+//   .then(response => console.log(response.results));
 
-var chefs =
-  'Chefs. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.';
+const data = [
+  {
+    section: 'cuisines',
+    story:
+      'Cuisines. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio maiores adipisci quibusdam repudiandae dolor vero placeat esse sit! Quibusdam saepe aperiam explicabo placeat optio, consequuntur nihil voluptatibus expedita quia vero perferendis, deserunt et incidunt eveniet temporibus doloremque possimus facilis.',
+  },
+  {
+    section: 'chefs',
+    story:
+      'Chefs. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.',
+  },
+  {
+    section: 'reviews',
+    story:
+      'Reviews. Asperiores laudantium, rerum ratione consequatur, culpa consectetur possimus atque ab tempore illum non dolor nesciunt. Neque, rerum. A vel non incidunt, quod doloremque dignissimos necessitatibus aliquid laboriosam architecto at cupiditate commodi expedita in, quae blanditiis.',
+  },
+  {
+    section: 'delivery',
+    story:
+      'Delivery. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.',
+  },
+];
 
-var reviews =
-  'Reviews. Asperiores laudantium, rerum ratione consequatur, culpa consectetur possimus atque ab tempore illum non dolor nesciunt. Neque, rerum. A vel non incidunt, quod doloremque dignissimos necessitatibus aliquid laboriosam architecto at cupiditate commodi expedita in, quae blanditiis.';
+const contentPara = document.querySelector('.content');
+contentPara.innerHTML = data[0].story;
 
-var delivery =
-  'Delivery. Possimus labore, officia dolore! Eaque ratione saepe, alias harum laboriosam deserunt laudantium blanditiis eum explicabo placeat reiciendis labore iste sint. Consectetur expedita dignissimos, non quos distinctio, eos rerum facilis eligendi.';
-
-
-var contentPara = document.querySelector('.content');
-contentPara.innerHTML = cuisines;
-
-var tabs = document.querySelectorAll('.nav a');
+const tabs = document.querySelectorAll('.nav a');
 tabs.forEach(tab => tab.addEventListener('click', makeActive));
 
 function makeActive() {
-  console.log(this.href);
   makeInactive();
   event.target.classList.add('active');
-  if (this.href.includes('cuisines')) {
-    contentPara.innerHTML = cuisines;
-  } else if (this.href.includes('chefs')) {
-    contentPara.innerHTML = chefs;
-  } else if (this.href.includes('reviews')) {
-    contentPara.innerHTML = reviews;
-  } else if (this.href.includes('delivery')) {
-    contentPara.innerHTML = delivery;
+  let activePage = document.querySelector('.active');
+  let storyRef = activePage.dataset.story;
+  // console.log(storyRef);
+
+  for (let i = 0; i < data.length; i++) {
+    if (data[i].section === storyRef) {
+      // console.log(data[i].story);
+      contentPara.innerHTML = data[i].story;
+      makeHeader(storyRef);
+    }
   }
   event.preventDefault();
+}
+
+function makeHeader(head) {
+  const myHeader = document.createElement('h3');
+  myHeader.innerText = head;
+  console.log(myHeader);
+  contentPara.prepend(myHeader);
 }
 
 function makeInactive() {
